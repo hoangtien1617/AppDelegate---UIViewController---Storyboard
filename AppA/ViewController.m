8945331a6962cecd85a15c4e2a45893b896918f7
Hoàng Tiến <hoangtien1617@gmail.com>
2015-11-17 14:22:02 +0700

    no message

---
+++ AppA/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  AppA
+//
+//  Created by Hoàng Tiến on 11/17/15.
+//  Copyright © 2015 Hoàng Tiến. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    
+    
+}
+- (IBAction)openAppB:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"testOpenAppB://"]];
+}
+
+@end
---
+++ AppB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppB
+//
+//  Created by Hoàng Tiến on 11/17/15.
+//  Copyright © 2015 Hoàng Tiến. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
